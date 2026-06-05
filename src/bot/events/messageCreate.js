c420e45ca5a0092c087afca74ea1248f5a951bc6
zarpil <zarpil.ro@gmail.com>
2026-06-05 11:13:32 +0200

    fix :panel multilenguage

--- src/bot/events/messageCreate.js
+++ src/bot/events/messageCreate.js
@@ -5,6 +5,7 @@ const { getManager } = require('../../lavalink/manager');
 const { ensurePlayer } = require('../utils/voiceUtils');
 const { syncState } = require('../../utils/stateSync');
 const db = require('../../db/database');
+const { t } = require('../../utils/i18n');
 
 module.exports = {
     name: Events.MessageCreate,
@@ -32,7 +33,7 @@ module.exports = {
             const res = await player.search(queryClean, message.author);
             
             if (res.loadType === 'empty' || res.loadType === 'error') {
-                const tempMsg = await message.channel.send(`❌ No se encontró nada para \`${query}\``);
+                const tempMsg = await message.channel.send(t(message.guildId, 'errors.noResultsSimple', { query }));
                 setTimeout(() => tempMsg.delete().catch(() => {}), 5000);
                 return;
             }
@@ -55,7 +56,7 @@ module.exports = {
             // The panel will be updated by the manager events (trackStart, etc)
         } catch (err) {
             console.error('[MessageCreate] Setup channel error:', err);
-            const tempMsg = await message.channel.send(`❌ Error: ${err.message}`);
+            const tempMsg = await message.channel.send(t(message.guildId, 'errors.errorMsg', { message: err.message }));
             setTimeout(() => tempMsg.delete().catch(() => {}), 5000);
         }
     },
